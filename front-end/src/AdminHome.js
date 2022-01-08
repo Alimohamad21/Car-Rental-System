3e3c3a35e8046830671566613f9c4aab4dd4b964
Ali Mohamed <alimohamadyoussef21@gmail.com>
2022-01-08 20:03:55 +0200

    initializing admin branch

--- front-end/src/AdminHome.js
+++ front-end/src/AdminHome.js
@@ -3,7 +3,7 @@ import React from "react";
 function AdminHome() {
     return (
         <div className='adminHome'>
-            <h1>Admin Home!</h1>
+            <h1>Admin</h1>
         </div>
     );
 }
